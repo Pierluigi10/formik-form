78e2529c439a74e637af94fc01ff82b621d2adb5
pierluigi <pb@evergreen.de>
2022-09-22 16:35:50 +0200

    add useField hook from formik

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useField } from "formik";
 
 const CustomInput=({label, ...props}) => {
+  const [field, meta, helper] = useField(props);
+  console.log("field",field)
+  console.log("meta", meta)
+  console.log("helper", helper)
   return (
     <>
     <label>{label}</label>
-    <input {...props} />
+    <input {...field} {...props} />
     </>
   )
 }
