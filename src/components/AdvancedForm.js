cf12003fcd4fe09ea9dd85aca0befc4621d738b6
pierluigi <pb@evergreen.de>
2022-09-22 15:37:21 +0200

    use Formk as component

--- src/components/AdvancedForm.js
+++ src/components/AdvancedForm.js
@@ -1,9 +1,22 @@
+import { Formik } from "formik";
+
 const AdvancedForm = () => {
   return (
-    <form autoComplete="off">
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" placeholder="Enter your username" />
-    </form>
+    <Formik initialValues={{ name: "jared" }}>
+      {(props) => (
+        <form onSubmit={props.handleSubmit}>
+          <input
+            type="text"
+            onChange={props.handleChange}
+            onBlur={props.handleBlur}
+            value={props.values.name}
+            name="name"
+          />
+          {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </Formik>
   );
 };
 export default AdvancedForm;
