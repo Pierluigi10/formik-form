a8f452c93bcf8932acba048ed0cf180dda768c2f
pierluigi <pb@evergreen.de>
2022-09-22 13:55:30 +0200

    fix bug and show the errors and touched messages

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -7,27 +7,31 @@ const onSubmit = () => {
 
 const BasicForm = () => {
   // const formik = useFormik({
-  const { values, errors, handleBlur, handleChange, handleSubmit } = useFormik({
-    initialValues: {
-      email: "",
-      age: "",
-      password: "",
-      confirmPAssword: "",
-    },
-    validationSchema: basicSchema,
-    onSubmit,
-  });
-  console.log("errors",errors);
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: {
+        email: "",
+        age: "",
+        password: "",
+        confirmPassword: "",
+      },
+      validationSchema: basicSchema,
+      onSubmit,
+    });
+  console.log("errors", errors);
   return (
     <form onSubmit={handleSubmit} autoComplete="off">
       <label htmlFor="email">Email</label>
       <input
         value={values.email}
         onChange={handleChange}
+        onBlur={handleBlur}
         id="email"
         type="email"
         placeholder="Enter your email"
+        className={errors.email && touched.email ? "input-error" : ""}
       />
+      {errors.email && touched.email && <p className="error">{errors.email}</p>}
       <label htmlFor="age">age</label>
       <input
         value={values.age}
@@ -36,7 +40,9 @@ const BasicForm = () => {
         id="age"
         type="age"
         placeholder="Enter your age"
-      />{" "}
+        className={errors.age && touched.age ? "input-error" : ""}
+      />
+      {errors.age && touched.age && <p className="error">{errors.age}</p>}
       <label htmlFor="password">password</label>
       <input
         value={values.password}
@@ -45,16 +51,26 @@ const BasicForm = () => {
         id="password"
         type="password"
         placeholder="Enter your password"
-      />{" "}
-      <label htmlFor="confirmPassword">confirm ypur Password</label>
+        className={errors.password && touched.password ? "input-error" : ""}
+      />
+      {errors.password && touched.password && (
+        <p className="error">{errors.password}</p>
+      )}
+      <label htmlFor="confirmPassword">Confirm Password</label>
       <input
+        id="confirmPassword"
+        type="password"
+        placeholder="Confirm password"
         value={values.confirmPassword}
         onChange={handleChange}
         onBlur={handleBlur}
-        id="confirmPassword"
-        type="confirmPassword"
-        placeholder="confirm your Password"
+        className={
+          errors.confirmPassword && touched.confirmPassword ? "input-error" : ""
+        }
       />
+      {errors.confirmPassword && touched.confirmPassword && (
+        <p className="error">{errors.confirmPassword}</p>
+      )}
       <button type="submit">Submit</button>
     </form>
   );
